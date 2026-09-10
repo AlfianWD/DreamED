ad1121499ec9e467d8a66dc93d850c5b4c497be0
AlfianWD <carzyman01@gmail.com>
2026-09-10 10:50:44 +0700

    chore(inertia): add root application view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { createApp, h } from "vue";
+
+// const appName = import.meta.env.VITE_APP_NAME || "";
+
+createInertiaApp({
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.vue`,
+            import.meta.glob("./pages/**/*.vue"),
+        ),
+    setup({ App, el, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+        .use(plugin)
+        .mount(el);
+    },
+});
---
+++ resources/js/pages/homepage.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <h1>Hello, Good Afternoon</h1>
+</template>
