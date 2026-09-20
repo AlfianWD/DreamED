eb88f8d4fa768962f7bccebb0918b6e13fea32f5
AlfianWD <carzyman01@gmail.com>
2026-09-20 16:35:09 +0700

    feat(register-page): add and configure left panel with tailwindcss

--- resources/js/pages/auth/register.vue
+++ resources/js/pages/auth/register.vue
@@ -1,6 +1,23 @@
 <script setup></script>
 <template>
-    <div class="min-h-screen bg-white overflow-x-hidden">
-        Welcome in Register
+    <div class="min-h-screen bg-white overflow-x-hidden md:flex">
+        <!-- Left Panel -->
+        <div
+            class="hidden ml-20 md:flex md:1/2 min-h-screen items-center justify-center"
+        >
+            <div
+                class="bg-purple-300 w-[530px] h-[592px] shadow-2xl rounded-2xl flex flex-col items-center justify-center"
+            >
+                <img
+                    :src="illustrationLearn"
+                    alt="Ilustration-Learn"
+                    class="w-[350px] h-[350px] mb-10"
+                />
+                <span class="text-lg text-center"
+                    >Learn Faster and Anywhere with
+                    <strong>DreamED</strong></span
+                >
+            </div>
+        </div>
     </div>
 </template>
