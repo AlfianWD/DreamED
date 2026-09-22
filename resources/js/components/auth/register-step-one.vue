b785e1a94add9f95a09f77b3cacb904504d9174d
AlfianWD <carzyman01@gmail.com>
2026-09-23 00:19:47 +0700

    refactor(register-page) : extract basic info form into step components

---
+++ resources/js/components/auth/register-step-one.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="ml-2">
+        <form>
+            <div class="flex flex-row mb-4">
+                <!-- First Name -->
+                <div class="mb-4">
+                    <label
+                        class="block text-[20px] font-semibold mb-4"
+                        for="first-name"
+                    >
+                        First Name
+                    </label>
+                    <input
+                        id="first-name"
+                        class="shadow border border-amber-500 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-none"
+                        type="text"
+                    />
+                </div>
+
+                <!-- Last Name -->
+                <div class="mb-4">
+                    <label
+                        class="block text-[20px] font-semibold mb-4"
+                        for="last-name"
+                    >
+                        Last Name
+                    </label>
+                    <input
+                        id="last-name"
+                        class="shadow border border-amber-500 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-none"
+                        type="text"
+                    />
+                </div>
+            </div>
+
+            <!-- Email -->
+            <div class="mb-4">
+                <label class="block text-[20px] font-semibold mb-4" for="email"
+                    >Email</label
+                >
+                <input
+                    id="email"
+                    class="shadow border border-amber-500 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-none"
+                    type="email"
+                    placeholder="Enter your email address"
+                />
+            </div>
+        </form>
+    </div>
+</template>
